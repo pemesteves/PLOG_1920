693a27920e408541ba24633fbc464f15c69f14c4
Pedro Esteves <up201705160@fe.up.pt>
2019-10-07 22:07:31 +0100

    Finished exercise 6

--- week3/exercise6.pl
+++ week3/exercise6.pl
@@ -4,8 +4,19 @@ delete_one(X, L1, L2) :-
     append(La, Lb, L2).
 
 %b
-delete_all(X, L1, L1) :- \+append(_, [X|_], L1).
-    
-delete_all(X, L1, L2) :-
-    delete_one(X, L1, L3),
-    delete_all(X, L3, L2). 
+delete_all(_, [], []).
+
+delete_all(X, [X|Xs], Y) :- delete_all(X, Xs, Y).
+
+delete_all(X, [T|Xs], [T|Y]) :-
+    X \= T,
+    delete_all(X, Xs, Y).
+
+%c
+delete_all_list([], _, _).
+
+delete_all_list([X | Lx], L1, L2) :-
+    delete_all(X, L1, Z), 
+    delete_all_list(Lx, Z, L2), 
+    (var(L2), L2 = Z;
+     nonvar(L2)).
