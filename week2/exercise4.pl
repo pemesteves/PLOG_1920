cb5cf1f27cc32713c9959f34ae5f0e4e8cf6c3a3
Pedro Esteves <up201705160@fe.up.pt>
2019-10-08 19:17:52 +0100

    Added exercises 4 and 5 from week 2

--- week2/exercise4.pl
+++ week2/exercise4.pl
@@ -6,6 +6,19 @@ factorial(N, Value) :-
     N1 is N - 1, factorial(N1, V1),
     Value is N * V1.
 
+%Other solution:
+factorial2(0, 1).
+factorial2(1, 1).
+factorial2(N, V) :-
+    fact(N, V, 1).
+
+fact(1, V, V).
+fact(N, V, Acc) :- 
+    N > 1,
+    N1 is N - 1,
+    Acc1 is Acc * N,
+    fact(N1, V, Acc1).
+
 %b)
 fibonacci(0,1).
 fibonacci(1,1).
